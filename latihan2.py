a92a40212061212d32998f03dc108e3996182e23
zulfahm <46733940+zulfahm@users.noreply.github.com>
2019-02-19 13:00:40 +0700

    Add files via upload

---
+++ latihan2.py
@@ -0,0 +1,10 @@
+max=0
+while True:
+        a=int(input("Masukan Bilangan = "))
+        print("")
+        if max < a:
+                max = a
+        print("Bilangan Terbesar = ",max)
+        if a==0:
+                break
+        print("")    
